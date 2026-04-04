e17c1b10551f0c92aee4abfa74e5fa1c64421b31
SrLordGrim <127856317+SrLordGrim@users.noreply.github.com>
2026-04-04 16:06:48 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.USE_PATH_PREFIX === 'true' ? '/biovis/' : '/',
+  base: '/VisMol/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
